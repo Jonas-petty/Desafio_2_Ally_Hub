51ce6baa1df95df4f4d53f17bfe5f9f7acca9906
Jonas Felix <jonaspetty@hotmail.com>
2022-10-30 21:44:39 -0300

    Fixing some problems in the code and adding some more comments to make it more understandable

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form'
 
-// import Input from './components/Input/index';
-
 function App() {
+  // Estados referentes as informações de País e Cidade
   const [pais, setPais] = useState([])
   const [cidade, setCidade] = useState([])
 
+  // Desestruturação das funções da biblioteca react-hook-form
   const { register, handleSubmit, formState: {errors} } = useForm()
 
   // Faz as requisições para as APIs
@@ -43,6 +43,7 @@ function App() {
         <div className="content">
           <div className="personalInfo">
             <h2>Dados Pessoais</h2>
+            {/* Acompanha todos inputs e adiciona suas validações */}
             <input {...register("nome", {required: "Digite seu nome."})} type="text" placeholder='Nome' />
             <p>{errors.nome?.message}</p>
 
@@ -66,12 +67,14 @@ function App() {
           <h2>Destinos de Interesse</h2>
             <select {...register('pais', {required: "Escolha um País."})}>
               <option value="">-- Escolha --</option>
+              {/* Adiciona a lista de tags <option> com as informações retornadas na API e tradatas */}
               {paises}
             </select>
             <p>{errors.pais?.message}</p>
 
             <select {...register('cidade', {required: "Escolha uma Cidade."})}>
               <option value="">-- Escolha --</option>
+              {/* Adiciona a lista de tags <option> com as informações retornadas na API e tradatas */}
               {cidades}
             </select>
             <p>{errors.cidade?.message}</p>
@@ -83,46 +86,6 @@ function App() {
       
       </form>
     </div>
-
-    // <form onSubmit={handleSubmit(data => {
-    //   console.log(data)
-    // })}>
-    //   <h2>Dados Pessoais</h2>
-    //          <input {...register("nome", {required: "Digite seu nome."})} type="text" placeholder='Nome' />
-    //          <p>{errors.nome?.message}</p>
-
-    //          <input {...register("email", {required: "Digite seu Email"})} type="email" placeholder='Email' />
-    //          <p>{errors.email?.message}</p>
-
-    //          <input {...register("tel", {required: "Digite seu Telefone"})} type="tel" placeholder='Telefone' />
-    //          <p>{errors.tel?.message}</p>
-
-    //          <input {...register("cpf", {required: "Digite seu CPF", minLength: {
-    //            value: 11,
-    //            message: "O CPF deve conter 11 números."
-    //          }, maxLength: {
-    //            value: 11,
-    //            message: "O CPF deve conter 11 números."
-    //          }})} type="text" placeholder='CPF' />
-    //          <p>{errors.cpf?.message}</p>
-
-    //          <h2>Destinos de Interesse</h2>
-    //          <select {...register('pais', {required: "Escolha um País."})}>
-    //            <option value="">-- Escolha --</option>
-    //            {paises}
-    //          </select>
-    //          <p>{errors.pais?.message}</p>
-
-    //          <select {...register('cidade', {required: "Escolha uma Cidade."})}>
-    //            <option value="">-- Escolha --</option>
-    //            {cidades}
-    //          </select>
-    //          <p>{errors.cidade?.message}</p>
-
-
-
-    //          <button type="submit">Enviar</button>
-    // </form>
   )
 }
 
